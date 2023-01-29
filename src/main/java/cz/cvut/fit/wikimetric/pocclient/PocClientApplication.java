44a1bf2354dbed043c41f11441da5bdf064224da
Ivo Kořínek <korinivo@fit.cvut.cz>
2023-01-29 17:01:22 +0100

    Initial commit

---
+++ src/main/java/cz/cvut/fit/wikimetric/pocclient/PocClientApplication.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.wikimetric.pocclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/cvut/fit/wikimetric/pocclient/PocClientApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.cvut.fit.wikimetric.pocclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
